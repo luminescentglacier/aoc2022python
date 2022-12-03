4f2bcf36f1812142f201f103ce4616446239c565
luminescentglacier <luminescentglacier@gmail.com>
2022-12-03 11:44:33 +0600

    day 3

---
+++ day3/main.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import Iterable
+
+
+def chunks(iterable: Iterable[str], size: int) -> Iterable[tuple[str, ...]]:
+    args = [iter(iterable)] * size
+    return zip(*args, strict=True)
+
+
+def letter_score(letter: str) -> int:
+    return ord(letter) - (96 if letter.islower() else 38)
+
+
+def part_1(s: str) -> int:
+    total = 0
+    for sack in s.splitlines():
+        middle = int(len(sack) / 2)
+        common = set(sack[:middle]).intersection(sack[middle:]).pop()
+        total += letter_score(common)
+    return total
+
+
+def part_2(s: str) -> int:
+    total = 0
+    for sacks in chunks(s.splitlines(), size=3):
+        contents = (set(sack) for sack in sacks)
+        common = set.intersection(*contents).pop()
+        total += letter_score(common)
+    return total
+
+
+if __name__ == "__main__":
+    s = Path("input.txt").read_text()
+    print(f"Part 1: {part_1(s)}")
+    print(f"Part 2: {part_2(s)}")
---
+++ day3/test.py
@@ -0,0 +1,17 @@
+import pytest
+from pathlib import Path
+
+from main import part_1, part_2
+
+
+@pytest.fixture
+def puzzle_input():
+    return Path("test.txt").read_text()
+
+
+def test_part_1(puzzle_input):
+    assert part_1(puzzle_input) == 157
+
+
+def test_part_2(puzzle_input):
+    assert part_2(puzzle_input) == 70
