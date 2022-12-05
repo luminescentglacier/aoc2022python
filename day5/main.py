b4816b13f118b7ca03a3ecb4f2b8d6072f68378e
luminescentglacier <luminescentglacier@gmail.com>
2022-12-06 00:16:08 +0600

    Fix parser for day 5

--- day5/main.py
+++ day5/main.py
@@ -1,43 +1,42 @@
 from pathlib import Path
+from typing import Iterable, Sequence
 
 
-def initial_positions(s: str) -> list[list[str]]:
-    for i, line in enumerate(s.splitlines()):
-        if line.startswith(" 1"):
-            stacks_num = int(line.split()[-1])
-            index_row = i
-            break
-    else:
-        raise ValueError("Unable to find crates number")
+def parse(s: str) -> tuple[list[list[str]], Iterable[tuple[int, int, int]]]:
+    crates_raw, instructions_raw = s.split("\n\n")
+    stacks = parse_stacks(crates_raw.splitlines())
+    instructions = parse_instructions(instructions_raw.splitlines())
+    return stacks, instructions
 
+
+def parse_stacks(lines: Sequence[str]) -> list[list[str]]:
+    stacks_num = (len(lines[0]) + 1) // 4
     stacks = [[] for _ in range(stacks_num)]
-    for line in s.splitlines()[:index_row]:
+    for line in lines:
         for i, crate in enumerate(line[1::4]):
             if crate != " ":
                 stacks[i].insert(0, crate)
     return stacks
 
 
-def instructions(s: str) -> tuple[int, int, int]:
-    for line in s.splitlines():
-        if not line.startswith("move"):
-            continue
+def parse_instructions(lines: Iterable[str]) -> Iterable[tuple[int, int, int]]:
+    for line in lines:
         words = line.split()
         count, src, dst = int(words[1]), int(words[3]) - 1, int(words[5]) - 1
         yield count, src, dst
 
 
 def part_1(s: str) -> str:
-    stacks = initial_positions(s)
-    for count, src, dst in instructions(s):
+    stacks, instructions = parse(s)
+    for count, src, dst in instructions:
         for _ in range(count):
             stacks[dst].append(stacks[src].pop())
     return "".join(s[-1] for s in stacks)
 
 
 def part_2(s: str) -> str:
-    stacks = initial_positions(s)
-    for count, src, dst in instructions(s):
+    stacks, instructions = parse(s)
+    for count, src, dst in instructions:
         stacks[dst].extend(stacks[src][-count:])
         stacks[src] = stacks[src][:-count]
     return "".join(s[-1] for s in stacks)
