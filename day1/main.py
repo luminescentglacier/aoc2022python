69f83706f9ebc7a56f0ea0cf27460c150ea6487e
luminescentglacier <luminescentglacier@gmail.com>
2022-12-06 00:37:09 +0600

    Rewrite day 1 using template

--- day1/main.py
+++ day1/main.py
@@ -1,38 +1,27 @@
+from pathlib import Path
 from typing import Iterable
 import bisect
-import sys
-
-try:
-    FILE = sys.argv[1]
-except IndexError:
-    FILE = "test.txt"
 
 
-def get_elfs() -> Iterable[int]:
-    elf = 0
-    with open(FILE) as file:
-        for line in file.read().splitlines():
-            if line:
-                elf += int(line)
-            else:
-                yield elf
-                elf = 0
+def get_elfs(s: str) -> Iterable[int]:
+    for elf in s.split("\n\n"):
+        yield sum(map(int, elf.splitlines()))
 
 
-def part_1() -> int:
-    return max(get_elfs())
+def part_1(s: str) -> int:
+    return max(get_elfs(s))
 
 
-def part_2() -> int:
+def part_2(s: str) -> int:
     top = 3
     leaders = []
-    for elf in get_elfs():
+    for elf in get_elfs(s):
         bisect.insort_right(leaders, elf)
         leaders = leaders[-top:]
     return sum(leaders)
 
 
 if __name__ == "__main__":
-    print(f"Input: {FILE}")
-    print(f"Part 1: {part_1()}")
-    print(f"Part 2: {part_2()}")
+    s = Path("input.txt").read_text()
+    print(f"Part 1: {part_1(s)}")
+    print(f"Part 2: {part_2(s)}")
---
+++ day1/test.py
@@ -0,0 +1,17 @@
+import pytest
+from pathlib import Path
+
+from main import part_1, part_2
+
+
+@pytest.fixture
+def puzzle_input():
+    return Path("test.txt").read_text()
+
+
+def test_part_1(puzzle_input):
+    assert part_1(puzzle_input) == 24000
+
+
+def test_part_2(puzzle_input):
+    assert part_2(puzzle_input) == 45000
